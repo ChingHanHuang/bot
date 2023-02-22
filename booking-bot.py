cb3dcc542cef1ce4e2812fae038de984a08d99c6
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-02-23 01:42:37 +0800

    Add webdriver chrome options

--- booking-bot.py
+++ booking-bot.py
@@ -2,13 +2,17 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 # parameters
 site_url = 'https://www.morningbrew.com/daily'
- 
-driver = webdriver.Chrome(service=Service('/vboxuser/repos/driver'))
+chrome_service = Service('/vboxuser/repos/driver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('start-maximized')
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 driver.get("https://www.theodinproject.com/lessons/foundations-html-boilerplate")
+
 time.sleep(3)
 print('Title: %s' % driver.title)
 #driver.quit()

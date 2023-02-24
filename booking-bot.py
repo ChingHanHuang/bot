06e5c4e284f7f2c7a51202cd0c6dcf35d3849766
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-02-25 00:51:46 +0800

    Open Taipei Pass login page and enter login info
    At the beginning, I use the detach experimental option of ChromeOptions
    to prevent window closing immediately and set window to maximum size.
    
    In every page, I use WebDriverWait with expected_conditions module to
    wait for specific element, which we want to interact with, to be
    clickable. In this way, we can avoid "element is not found" error.
    
    I use a json file to store login information, and extract the data when
    I need to enter account and password. This file is only stored in local
    directory.
    
    Create .gitignore file to add login_info.json file, in case I
    push this file to GitHub.

--- booking-bot.py
+++ booking-bot.py
@@ -1,18 +1,75 @@
 import time
+import json
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-# parameters
-site_url = 'https://www.morningbrew.com/daily'
-chrome_service = Service('/vboxuser/repos/driver')
+
+# Parameters
+SITE_URL = "https://gisweb.taipei.gov.tw/TPCamp/Default.aspx"
+LOGIN_INFO = "login_info.json"
+ACCOUNT = "account"
+PASSWORD = "password"
+
+# Launch browser
+chrome_service = Service("/vboxuser/repos/driver")
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('start-maximized')
+chrome_options.add_argument("start-maximized")
+chrome_options.add_experimental_option("detach", True)
+# chrome_options.add_argument("--user-data-dir=/home/vboxuser/.config/google-chrome")
+# chrome_options.add_argument("--profile-directory=Profile 3")
+
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+driver.get(SITE_URL)
 
-driver.get("https://www.theodinproject.com/lessons/foundations-html-boilerplate")
+# Open taipei pass login page
+wait = WebDriverWait(driver, 1)
+wait.until(EC.element_to_be_clickable((By.ID, "link_login")))
+driver.find_element(By.ID, "link_login").click()
 
-time.sleep(3)
-print('Title: %s' % driver.title)
-#driver.quit()
+wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "登入台北通")))
+driver.find_element(By.LINK_TEXT, "登入台北通").click()
+
+wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@href='./taipeipass']")))
+driver.find_element(By.XPATH, "//a[@href='./taipeipass']").click()
+
+# Extract account/password from configration file and login
+f = open(LOGIN_INFO)
+data = json.load(f)
+
+wait.until(EC.element_to_be_clickable((By.ID, ACCOUNT))) 
+account = driver.find_element(By.ID, ACCOUNT)
+account.clear()
+account.send_keys(data["taipei_pass"][0][ACCOUNT])
+password = driver.find_element(By.ID, "pass")
+password.clear()
+password.send_keys(data["taipei_pass"][0][PASSWORD])
+driver.find_element(By.LINK_TEXT, "登入").click()
+
+f.close()
+
+# Book Bishan camping area
+wait.until(EC.element_to_be_clickable((By.ID, "MainContent_BT_BishanBooking"))) 
+driver.find_element(By.ID, "MainContent_BT_BishanBooking").click()
+
+wait.until(EC.element_to_be_clickable((By.ID, "MainContent_TB_InTime_general"))) 
+# campTime = driver.find_element(By.ID, "MainContent_TB_InTime_general")
+# campTime.clear()
+# campTime.send_keys("2023/03/03")
+
+numOfPpl = driver.find_element(By.ID, "MainContent_TB_TNum_general")
+numOfPpl.clear()
+numOfPpl.send_keys("2")
+
+
+
+# close browser
+# driver.quit()
+
+
+# extract config file and input data
+# sleap vs webdriver wait
+# How to insert date

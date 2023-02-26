58dce28ea82d55e779d98de35d9d55b3b14bcb6f
Ching Han <120117415+ChingHanHuang@users.noreply.github.com>
2023-02-27 01:29:09 +0800

    Choose camping area by configuration file
    I create a json file to store all camping areas I would like to book. The area I really want, I would put it at the first position in configuration file.
    So if the first option has already booked by others, I will iterate the configuration, and check if I can book the second one I want. Repeat thest action
    untill all area options are checked.
    
    Click the agreement button.

--- booking-bot.py
+++ booking-bot.py
@@ -15,15 +15,13 @@ ACCOUNT = "account"
 PASSWORD = "password"
 CAMPING_DATE = "16"
 NUM_OF_PPL = "2"
-AREA_OPTIONS = ["area_H56", "area_H78", "area_H34", "area_H12", "area_J2", "area_J4"]
+AREA_OPTIONS = "camping_bot_config.json"
 
 # Launch browser
 chrome_service = Service("/vboxuser/repos/driver")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("start-maximized")
 chrome_options.add_experimental_option("detach", True)
-# chrome_options.add_argument("--user-data-dir=/home/vboxuser/.config/google-chrome")
-# chrome_options.add_argument("--profile-directory=Profile 3")
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 driver.get(SITE_URL)
@@ -55,7 +53,7 @@ driver.find_element(By.LINK_TEXT, "登入").click()
 f.close()
 
 # Book Bishan camping area
-time.sleep(2)
+time.sleep(0.5)
 wait.until(EC.element_to_be_clickable((By.ID, "MainContent_BT_BishanBooking"))) 
 driver.find_element(By.ID, "MainContent_BT_BishanBooking").click()
 
@@ -64,15 +62,47 @@ driver.find_element(By.ID, "MainContent_TB_InTime_general").click()
 wait.until(EC.element_to_be_clickable((By.LINK_TEXT, CAMPING_DATE))) 
 driver.find_element(By.LINK_TEXT, CAMPING_DATE).click()
 
-numOfPpl = driver.find_element(By.ID, "MainContent_TB_TNum_general")
-numOfPpl.clear()
-numOfPpl.send_keys(NUM_OF_PPL)
+num_of_people = driver.find_element(By.ID, "MainContent_TB_TNum_general")
+num_of_people.clear()
+num_of_people.send_keys(NUM_OF_PPL)
 
+# Extract camping area options from configration file
+wait.until(EC.element_to_be_clickable((By.ID, "Map_camp")))
 
+f = open(AREA_OPTIONS)
+area_options = json.load(f)
+for a in area_options:
+    area_number = a.split("_")[1]
+    area = driver.find_element(By.ID, a)
+    if area.get_attribute("title") == "尚有空位":
+        area.click()
+        if "options" in area_options[a]:
+            close_btn_xpath = f"//*[@id='Modal_{area_number}']/div/div/div[1]/button"
+            wait.until(EC.element_to_be_clickable((By.XPATH, close_btn_xpath)))
+            for option in area_options[a]["options"]:
+                camping_number = option.split("_")[1]
+                if driver.find_element(By.ID, option).get_attribute("title") == "尚有空位":
+                    camping_num_btn_xpath = f"//*[@id='area_{camping_number}']/div/button"
+                    driver.find_element(By.XPATH, camping_num_btn_xpath).click()
+                    obj = driver.switch_to.alert
+                    obj.accept()
+                    break
+            driver.find_element(By.XPATH, close_btn_xpath).click()
+        else: 
+            break
+    if int(driver.find_element(By.XPATH, "//*[@id='div_totalCamp']").text) > 0:
+        break
+f.close()
+
+driver.find_element(By.XPATH,"//*[@id='ctl01']/div[3]/div[2]/div/div[6]/div/div[3]/div/div[1]/div/div/div[1]/label").click()
+
+# Submit the application
+# driver.find_element(By.XPATH, "//*[@id='MainContent_BT_Send_general']").click()
 
 # close browser
 # driver.quit()
 
 
-# extract config file and input data
+# how to handle TimeoutError 
+# wait till 12 PM to start to book
 # sleep vs webdriver wait
